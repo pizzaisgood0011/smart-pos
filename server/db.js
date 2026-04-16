716c71e75f770da59a78607bce7a652f4cb258fc
pizzaisgood0011 <bodaro@arch.localdomain>
2026-04-16 22:23:44 +0700

    Add enviroment variable for production API

--- server/db.js
+++ server/db.js
@@ -1,16 +1,32 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+// postgresql on local machine
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+// });
+
+// pool.connect()
+//   .then(()=> console.log('Connected to PostgreSQL'))
+//   .catch(err=>console.error('DB connection error:',err));
+
+
+// superbase
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: { rejectUnauthorized: false }
 });
 
 pool.connect()
-  .then(()=> console.log('Connected to PostgreSQL'))
-  .catch(err=>console.error('DB connection error:',err));
+  .then(() => console.log('Connected to Supabase PostgreSQL'))
+  .catch(err => console.error('DB connection error:', err));
 
 module.exports = pool;
